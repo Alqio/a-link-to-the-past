32fa337d1af0bccbeba28b4a4261213349d09a93
Elias Panula <elias.panula@gmail.com>
2022-01-29 22:13:42 +0200

    master Fix bug with past timer

--- fgj/Assets/Scripts/Timer/TimerUI.cs
+++ fgj/Assets/Scripts/Timer/TimerUI.cs
@@ -23,12 +23,14 @@ public class TimerUI : MonoBehaviour
         Debug.Log("OnEnterPast in TimerUi");
         barComponent.SetValue(GameManager.Instance.PAST_MAX_SECONDS);
         fillImage.color = new Color(255, 255, 20.0f);
+        barComponent.slider.maxValue = GameManager.Instance.PAST_MAX_SECONDS;
     }
 
     public void OnEnterFuture() {
         Debug.Log("OnEnterFuture in TimerUi");
         barComponent.SetValue(0);
         fillImage.color = originalColor;
+        barComponent.slider.maxValue = GameManager.Instance.COOLDOWN_MAX_SECONDS;
     }
 
     // Update is called once per frame
